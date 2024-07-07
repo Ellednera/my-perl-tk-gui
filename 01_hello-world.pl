4424736f541b02394720e6e9da1f466f2f910f5f
Raphael Jong Jun Jie <raphael@moonstone.ellednera.org>
2024-07-07 20:38:37 +0800

    First hello world program

---
+++ 01_hello-world.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+
+use Tk;
+
+my $window = MainWindow->new;
+
+$window->title( "Hello world!" );
+$window->Button( 
+    -text => "Close",
+    -command => \&close
+)->pack;
+
+MainLoop;
+
+sub close {
+    say "Program closed";
+    exit;
+}
+
+# besiyata d'shmaya
