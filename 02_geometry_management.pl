d51719a2ffcfdc69e08cee3c74c2d1f37396947c
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-09 11:18:54 +0800

    Bad window working

--- 02_geometry_management.pl
+++ 02_geometry_management.pl
@@ -5,19 +5,18 @@ use warnings;
 use 5.010;
 
 use Tk;
-use Tk::CheckButton;
 
 my $bad_window = MainWindow->new;
 my $nice_window = MainWindow->new;
 
 # calling pack without any options
 $bad_window->title( "Bad Window" );
-$bad_window->Label( -text => "List of Items Available" );
+$bad_window->Label( -text => "List of Items Available" )->pack;
 
-$bad_window->CheckButton( -text => "Pen" )->pack;
-$bad_window->CheckButton( -text => "Books" )->pack;
-$bad_window->CheckButton( -text => "Bottle" )->pack;
-$bad_window->CheckButton( -text => "Bread" )->pack;
+$bad_window->Checkbutton( -text => "Pen" )->pack;
+$bad_window->Checkbutton( -text => "Books" )->pack;
+$bad_window->Checkbutton( -text => "Bottle" )->pack;
+$bad_window->Checkbutton( -text => "Bread" )->pack;
 
 $bad_window->Button( 
     -text => "Exit", 
