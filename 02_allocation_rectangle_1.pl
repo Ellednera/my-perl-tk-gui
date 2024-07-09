9488a55b8d2ca597ff98851a2de00fad76cb7f9f
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-09 12:10:04 +0800

    minor changes

--- 02_allocation_rectangle_1.pl
+++ 02_allocation_rectangle_1.pl
@@ -11,8 +11,6 @@ my $window = MainWindow->new;
 
 $window->Button( -text => "Top" )->pack( -side => "top" );
 $window->Button( -text => "Bottom" )->pack( -side => "bottom" );
-
-
 $window->Button( -text => "Left" )->pack( -side => "left" );
 $window->Button( -text => "Right" )->pack( -side => "right" );
 
