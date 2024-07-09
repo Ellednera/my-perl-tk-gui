d6e60f86bb740f1b76b48c53a22470135d973d4d
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-09 12:09:01 +0800

    Allocation with '-side'

--- 02_allocation_rectangle_1.pl
+++ 02_allocation_rectangle_1.pl
@@ -7,15 +7,16 @@ use Tk;
 
 my $window = MainWindow->new;
 
+
+
 $window->Button( -text => "Top" )->pack( -side => "top" );
-$window->Button( -text => "Left" )->pack( -side => "left" );
 $window->Button( -text => "Bottom" )->pack( -side => "bottom" );
+
+
+$window->Button( -text => "Left" )->pack( -side => "left" );
 $window->Button( -text => "Right" )->pack( -side => "right" );
 
-$window->Button( 
-            -text => "Exit", 
-            -command => sub { exit } 
-        )->pack( -anchor => "c" );
+
 
 MainLoop;
 
