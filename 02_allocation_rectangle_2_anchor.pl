e8b267a651e55fc84a5fae7b192771cd7c61811a
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-09 12:38:09 +0800

    some internal padding

--- 02_allocation_rectangle_2_anchor.pl
+++ 02_allocation_rectangle_2_anchor.pl
@@ -8,15 +8,36 @@ use Tk;
 my $window = MainWindow->new;
 
 
-$window->Button( -text => "LEFT" )->pack( -side => "left", -fill => "y" );
-$window->Button( -text => "TOP" )->pack( -side => "top", -fill => "x" );
-$window->Button( -text => "RIGHT" )->pack( -side => "right", -fill => "y" );
-$window->Button( -text => "BOTTOM" )->pack( -side => "bottom", -fill => "x" );
+$window->Button( 
+             -text => "LEFT"
+         )->pack(
+             -side => "left", -fill => "y", -ipadx => 20
+         );
+         
+$window->Button(
+             -text => "TOP"
+         )->pack(
+             -side => "top", -fill => "x",  -ipady => 20
+         );
+         
+$window->Button( 
+             -text => "RIGHT" 
+         )->pack( 
+             -side => "right", -fill => "y", -ipadx => 20
+         );
+$window->Button( 
+             -text => "BOTTOM"
+         )->pack(
+             -side => "bottom", -fill => "x", -ipady => 20
+         );
 
 # -anchor => center must have -expand => 1
 $window->Button( 
              -text => "EXIT", -command => sub {exit} 
-         )->pack( -anchor => "c", -expand => 1, -fill => "both" );
+         )->pack( 
+             -anchor => "c", -expand => 1, -fill => "both",
+             -ipadx => 50, -ipady => 50
+         );
 
 MainLoop;
 
