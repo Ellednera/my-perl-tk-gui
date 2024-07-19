b3285699e5a7a41cfb2b5a9b2d387974df6218d3
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-19 22:45:46 +0800

    add buttons

--- 02_form.pl
+++ 02_form.pl
@@ -5,8 +5,18 @@ use warnings;
 
 use Tk;
 
-
 my $window = MainWindow->new();
+
+for ( 1..5 ) {
+    $window->Button( 
+        -text => $_ * 7 
+    )->form(
+        -left => "%" . $_ * 10,
+        -top => "%" . $_ * 5
+    );
+}
+
+# Buttons at (10, 5), (20, 10), (30, 15), (40, 20), (50, 25)
 
 MainLoop;
 
