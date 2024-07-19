4b564d257c6e998a7bda62980e262c88c915582d
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-19 21:03:31 +0800

    anchor

--- 02_place.pl
+++ 02_place.pl
@@ -7,6 +7,23 @@ use Tk;
 
 my $window = MainWindow->new;
 
+$window->Button(
+    -text => "Exit",
+    -command => sub { exit }
+)->place( -x => 5, "-y" => 5 );
+
+$window->Button(
+    -text => "Exit",
+    -command => sub { exit }
+)->place( -x => 25, "-y" => 10 );
+
+$window->Button( 
+    -text => "Relative place"
+)->place( -relx => 0.5, -rely => 0.5, -anchor => "center" );
+
+$window->Button( 
+    -text => "-anchor => sw" 
+)->place( -x => 100, "-y" => 150, -anchor => "sw" );
 
 MainLoop;
 # besiyata d'shmaya
