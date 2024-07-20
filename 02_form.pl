104be13f0808645f61d0bb53395361f82979494c
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-20 11:39:04 +0800

    simple attachment

--- 02_form.pl
+++ 02_form.pl
@@ -18,6 +18,16 @@ for ( 1..5 ) {
 
 # Buttons at (10, 5), (20, 10), (30, 15), (40, 20), (50, 25)
 
+my $button_1 = $window->Button( -text => "left" )->form( -left => "%15", -top => "%75" );
+
+$window->Button( 
+    -text => "right" 
+)->form( 
+    -left => $button_1, -right => "%70", 
+    -top => "%0", -bottom => "%80" 
+);
+
+
 MainLoop;
 
 # besiyata d'shmaya
