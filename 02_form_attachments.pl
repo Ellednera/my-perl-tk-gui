9cd10ff96a98493d5e368dd06f5b9e520c15fc8c
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-20 11:52:20 +0800

    attachment working

---
+++ 02_form_attachments.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Tk;
+
+my $window = MainWindow->new;
+
+my $btn_1 = $window->Button( -text => "small" )->form( -left => "%10" );
+
+$window->Button(
+    -text => "big"
+)->form( 
+    -left => $btn_1, -right => "%70",
+    -top => "%25", -bottom => "%80"
+);
+
+MainLoop;
+
+# besiyata d'shmaya
