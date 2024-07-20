405204b9192aade06ec0f76fd4df7761a2ed614f
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-20 12:00:27 +0800

    minor modifications

--- 02_form_attachments.pl
+++ 02_form_attachments.pl
@@ -12,7 +12,7 @@ my $btn_1 = $window->Button( -text => "small" )->form( -left => "%10" );
 $window->Button(
     -text => "big"
 )->form( 
-    -left => $btn_1, -right => "%70",
+    -left => [$btn_1, 20], -right => "%70",
     -top => "%25", -bottom => "%80"
 );
 
