98456cefe9ad7e2fb5fb5a71fe5d7e074b673219
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-23 21:56:04 +0800

    font family, size, weight & style

---
+++ chapter_3/03_1_font-viewer.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Tk;
+use Tk::BrowseEntry;
+
+my $font_family = "Courier";
+my $font_size = 12;
+my $font_weight = "normal";
+my $font_style = "roman";
+
+my $window = MainWindow->new( -title => "Font Viewer" );
+my $frame = $window->Frame->pack( -side => "top" );
+
+# font family selection
+my $font_family_be = $frame->BrowseEntry( 
+    -label => "Family: ", 
+    -variable => \$font_family,
+    -browsecmd => \&apply_font
+)->pack( -fill => "x", -side => "left" );
+
+$font_family_be->insert( "end", sort $window->fontFamilies );
+
+# font size selection
+my $font_size_be = $frame->BrowseEntry(
+    -label => "Size: ",
+    -variable => \$font_size,
+    -browsecmd => \&apply_font
+)->pack( -side => "left" );
+
+$font_size_be->insert( "end", (5 .. 80) );
+
+# font weight
+$frame->Checkbutton( 
+    -onvalue => "bold", -offvalue => "normal",
+    -text => "Weight",
+    -variable => \$font_weight,
+    -command => \&apply_font
+)->pack( -side => "left" );
+
+# font style
+$frame->Checkbutton(
+    -onvalue => "italic", -offvalue => "roman",
+    -text => "Slant", -variable => \$font_style,
+    -command => \&apply_font
+)->pack( -side => "left" );
+
+MainLoop;
+
+sub apply_font {
+    ...
+}
+
+# besiyata d'shmaya
