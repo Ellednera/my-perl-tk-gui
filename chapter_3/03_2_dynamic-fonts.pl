c90102f91d6441ec043015196b8f3411543e483b
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-28 14:36:46 +0800

    Font viewer working

--- chapter_3/03_2_dynamic-fonts.pl
+++ chapter_3/03_2_dynamic-fonts.pl
@@ -13,6 +13,7 @@ $window->minsize( 700, 400 );
 my $tlist = $window->Scrolled( 
     "TList", 
     -font => [ "FreeSerif", "12" ],
+    -selectmode => "browse",
     -command => \&show_font
 )->pack(
     -fill => "both",
@@ -27,7 +28,22 @@ for ( sort $window->fontFamilies ) {
 MainLoop;
 
 sub show_font {
+    my $index = $_[0]; #print $index . "\n";
+    #print $_ . "\n" for @_;
+    #print $tlist->info;
+    my $name = $tlist->entrycget( $index, -text );
     
+    my $popup_window = $window->Toplevel( -title => $name );
+    my $text = $popup_window->Scrolled( 
+        "ROText", -wrap => "none" 
+    )->pack(
+        -expand => 1, -fill => "both"
+    );
+
+    # create a tag name, will be used later on
+    $text->tagConfigure( "number", -font => [ "FreeSerif", "12" ] );
+    $text->tagConfigure( "selected-font", -font => [ $name, "20" ] );
+    $text->insert( "end", "abcdefghijklmnopqrstuvwxyzABCDEFG...", "selected-font" );
     
     
 }
