40173ccffc5e9aca857c6ea29cc787ec71bc6d32
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2024-07-29 10:59:39 +0800

    buttons with bitmap done

--- chapter_4/04_1_image_button.pl
+++ chapter_4/04_1_image_button.pl
@@ -5,9 +5,22 @@ use warnings;
 
 use Tk;
 
+my @built_in_bitmaps = qw( error gray12 gray25 gray50 gray75 hourglass
+                           info questhead question warning Tk transparent );
 
 my $window = MainWindow->new();
 
+for ( @built_in_bitmaps ) {
+    $window->Button( 
+        -bitmap => $_,
+        -command => [ \&on_click, $_ ]
+    )->pack( -side => "left", -fill => "both", -expand => 1 );
+}
+
 MainLoop;
+
+sub on_click {
+    print "Button '", $_[0], "' clicked...\n";
+}
 
 # besiyata d'shamaya
